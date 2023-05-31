f5f9a12d734ef2b0b8d203ad5349dfaf31509f82
Arthur Gomes <109075801+ArthurGomes09@users.noreply.github.com>
2023-05-31 12:47:04 -0300

    Add files via upload

---
+++ provpratica ArthurGomesBarros/app.js
@@ -0,0 +1,14 @@
+const abrirModal = document.getElementById("abrirModal")
+const modal = document.getElementById("modal")
+const btnCora = document.querySelectorAll("#btn")
+
+abrirModal.addEventListener("click",()=>{
+    if(modal.style.display = none){
+        modal.style.display = flex
+    }
+})
+btnCora.forEach(addEventListener("click",()=>{
+    if(btnCora.previusSibling.fill ="none"){
+        btnCora.fill = "red"
+    }
+}))
